8676f8064f7ffb4d1b9bb62f36c1396db5bbb15d
Justin Winslow <justin@jwin.me>
2017-06-20 12:24:37 -0500

    Pagination

---
+++ index.js
@@ -0,0 +1,55 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  }
+}(this, function(angular, $) {
+  angular.module('Pagination', [])
+    .directive('pages', ['$compile', function($compile){
+      return {
+        compile: function(tElem, tAttrs){
+          var pages = tElem.find('page').detach();
+          return {
+            post: function(scope, el){
+              var currentPage;
+              var changePage = scope.changePage = function(page) {
+                if ($.isNumeric(page)) {
+                  // Do -1 so the API makes sense. In other words, changePage(2)
+                  // will get you actual page 2 even though its index is 1
+                  currentPage = (page - 1);
+                } else if (page == '+') {
+                  currentPage++;
+                } else if (page == '-') {
+                  currentPage--;
+                }
+                el.empty();
+                el.append($compile($(pages[currentPage]).clone())(scope));
+              };
+              changePage(1);
+              scope.next = function(){
+                scope.changePage('+');
+              };
+              scope.prev = function(){
+                scope.changePage('-');
+              };
+            }
+          };
+        }
+      };
+    }])
+    .directive('next', function(){
+      return {
+        replace: true,
+        template: '<button class="primary" ng-click="next()">Next</button>'
+      };
+    })
+    .directive('prev', function(){
+      return {
+        replace: true,
+        template: '<button ng-click="prev()">Previous</button>'
+      };
+    });
+
+  return angular.module('Pagination');
+}));
