67a545151b0d89d01c5b03ad6894839281bd3336
Justin Winslow <justin@jwin.me>
2019-05-10 10:49:16 -0500

    Fix issue where multiple pages could render

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ define(function(require){
   var angular = require('angular');
 
   angular.module('Pagination', [])
-    .directive('pages', ['$compile', function($compile){
+    .directive('pages', ['$compile', '$timeout', function($compile, $timeout){
       return {
         compile: function(tElem, tAttrs){
           var pages = $(tElem).find('> page').detach();
           return {
             post: function(scope, el){
-              var currentPage;
+              var currentPage, changing;
               var changePage = scope.changePage = function(page) {
+                // If the page is already changing, let's give it 
+                // a moment and try again. This gives the DOM time
+                // to do it's thing to multiple pages don't render 
+                // at once
+                if (changing) {
+                  return $timeout(function(){
+                    scope.changePage(page);
+                  }, 10);
+                }
+
+                changing = true;
+
                 if ($.isNumeric(page)) {
                   // Do -1 so the API makes sense. In other words, changePage(2)
                   // will get you actual page 2 even though its index is 1
@@ -22,6 +34,9 @@ define(function(require){
                 }
                 el.empty();
                 el.append($compile($(pages[currentPage]).clone())(scope));
+                $timeout(function(){
+                  changing = false;
+                }, 100);
               };
               changePage(1);
               scope.next = function(){
